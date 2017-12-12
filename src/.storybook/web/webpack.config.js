7087cc5bf928d1916a02cb759a0e1579b19c21c4
Burke Shartsis <burke@burkeshartsis.com>
2017-12-12 16:02:54 -0500

    WIP fix for compiling ES6 within node modules.

--- src/.storybook/web/webpack.config.js
+++ src/.storybook/web/webpack.config.js
@@ -1,6 +1,15 @@
 /* eslint-disable */
-module.exports = (storybookBaseConfig) => {
-  storybookBaseConfig.resolve = {
+
+
+// load the default config generator.
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+
+  console.log('BASE', config);
+
+  config.resolve = {
     modules: ['node_modules'],
     extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
@@ -9,10 +18,30 @@ module.exports = (storybookBaseConfig) => {
     }
   };
 
-  storybookBaseConfig.module.rules.push({
-    test: /\.css$/,
-    loader: 'style-loader!css-loader?url=false'
-  });
+  config.module.loaders = [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: [
+        'react-hot',
+        'babel-loader?cacheDirectory=true'
+      ]
+    },
+    {
+      // Most react-native libraries include uncompiled ES6 JS.
+      test: /\.js$/,
+      include: [/node_modules\/react-native-/, /node_modules\/rn-/],
+      loader: 'babel-loader',
+      query: { cacheDirectory: true }
+    },
+  ];
 
-  return storybookBaseConfig;
+  console.log('NEW', config);
+
+  // config.module.rules.push({
+  //   test: /\.css$/,
+  //   loader: 'style-loader!css-loader?url=false'
+  // });
+
+  return config;
 };
